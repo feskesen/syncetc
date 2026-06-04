b56ff758f252989ffa2c054a8601c342302a625f
feskesen <frank@syncetc.com>
2026-06-03 23:30:19 -0400

    Add files via upload

--- assets/core/CORE-COMPONENT-admin-shell-current.js
+++ assets/core/CORE-COMPONENT-admin-shell-current.js
@@ -7,7 +7,7 @@
 (function () {
   "use strict";
 
-  const VERSION = "2026-06-03-001";
+  const VERSION = "2026-06-03-006";
   const SHELL_ID = "syncetc-admin-shell";
   const LOGO_URL = "https://bxywokidhgppmlzyqvem.supabase.co/storage/v1/object/public/core-assets/SyncEtc-logo-compact.png";
 
